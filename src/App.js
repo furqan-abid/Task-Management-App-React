faef8d72b5dddd629076e06822d00e5350522087
furqan-abid <furqanabid10@gmail.com>
2023-07-30 15:40:52 +0500

    initialize react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskManagement from './Pages/TaskManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskManagement/>
+    </div>
+  );
+}
+
+export default App;
